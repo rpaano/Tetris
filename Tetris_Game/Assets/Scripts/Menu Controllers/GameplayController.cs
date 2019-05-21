510bc1242477556d812a0e2ce23604aa6f1cb2cb
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-05-21 23:58:16 +0800

    Play Pause, Quit, Resume

--- Tetris_Game/Assets/Scripts/Menu Controllers/GameplayController.cs
+++ Tetris_Game/Assets/Scripts/Menu Controllers/GameplayController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GameplayController : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject pausePanel;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pausePanel.SetActive(false);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PauseGame()
     {
-        
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel("MainMenu");
+#pragma warning restore CS0618 // Type or member is obsolete
     }
 }
--- Tetris_Game/Assets/Scripts/Menu Controllers/MainMenuController.cs
+++ Tetris_Game/Assets/Scripts/Menu Controllers/MainMenuController.cs
@@ -6,6 +6,8 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
+#pragma warning disable CS0618 // Type or member is obsolete
         Application.LoadLevel ("Gameplay");
+#pragma warning restore CS0618 // Type or member is obsolete
     }
 }
